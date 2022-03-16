8ad00c38352dd8701e5c0d1b57ec062b679b8214
Benshalom <benshalom.kariuki@kopokopo.com>
2022-03-16 18:43:12 +0300

    add authentication systems

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
   resources :students, except: [:destroy]
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'logout', to: 'logins#destroy'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
